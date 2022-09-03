e31ec5bedbe91df02933421d0b44ca33b950ae97
Tihsrah <harshlf4@gmail.com>
2022-09-03 11:02:25 +0530

    stop word checking with other dictionaries, applied binary and multi test on kaggle dataset

--- codes/minor_binary_test.py
+++ codes/minor_binary_test.py
@@ -1,5 +1,6 @@
 import re
 import string
+from tqdm import tqdm 
 import pandas as pd
 
 
@@ -9,23 +10,29 @@ pos_neg=pd.read_excel(r'C:\Users\HP OMEN\Downloads\pos_neg\Positive and Negative
 stop_word=pd.read_csv(r'C:\Users\HP OMEN\Downloads\stopwords-en-master\stopwords-en-master\stopwords-en.txt').iloc[:,0].tolist()
 neg=pos_neg['Negative Sense Word List'].dropna().tolist()
 pos=pos_neg['Positive Sense Word List'].dropna().tolist()
- 
+
+
 for i in stop_word:
     if i in pos:
         stop_word.remove(i)
 for i in stop_word:
     if i in neg:
-        stop_word.remove(i)  
+        stop_word.remove(i)    
 
-check_data=pd.read_csv(r'C:\Users\HP OMEN\Downloads\output.csv')
-check_data_val=check_data.iloc[:,1].tolist()
+check_data=pd.read_csv(r'C:\Users\HP OMEN\Downloads\sentiment_test\sentiment_no_emoticons.csv',encoding='latin-1')
+print(check_data.iloc[:,0].unique())
+check_data=check_data[check_data.iloc[:,0] != 2]
+print(len(check_data))
+# print(check_data[check_data['0'] != 2])
+print(check_data.iloc[:,0].unique())
+check_data_val=check_data.iloc[:,0].tolist()
+# print(check_data_val)
 
-test_data=check_data.iloc[:,0].tolist()
+test_data=check_data.iloc[:,5].tolist()
+# print(len(test_data))
 
 our_result=[]
-
-for i in range(len(test_data)):
-
+for i in tqdm(range(len(test_data))):
     # removal of url
     non_url=re.sub(r'''(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))''', " ", test_data[i])
     non_url = non_url.strip()
@@ -66,7 +73,9 @@ for i in range(len(test_data)):
     result = pos_count-neg_count
 
     if(result>0):
-        our_result.append(1)
+        our_result.append(4)
+    elif(result==0):
+        our_result.append(2)
     else:
         our_result.append(0)
 # print(check_data_val)
@@ -74,6 +83,7 @@ for i in range(len(test_data)):
 
 positive_prediction=0
 negative_prediction=0
+neutral_prediction=0
 for i in range(len(our_result)):
     if our_result[i]==check_data_val[i]:
         positive_prediction=positive_prediction+1
@@ -82,3 +92,4 @@ for i in range(len(our_result)):
 
 print(positive_prediction)
 print(negative_prediction)
+
---
+++ codes/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+ata_val=check_data.iloc[:,0].tolist()
+# print(check_data_val)
+
+# test_data=check_data.iloc[:,5].tolist()
+# print(test_data)
